9f4dfc69d2a42fff6a225600031d110265edde61
Арсений <arsunik07@mail.ru>
2025-11-29 15:02:44 +0500

    Начал работу над API для админ-панели

---
+++ Controllers/WebClientController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ChatBot_Inpad_server.Controllers
+{
+    [ApiController]
+    [Route("api/web")]
+    public class WebClientController : ControllerBase
+    {
+        private readonly DbContext db;
+
+        [HttpGet("test")]
+        public IActionResult TestMethod()
+        { 
+            return Ok("Работает");
+        }
+
+        [HttpPost("questions/add")]
+        public async IActionResult AddQuestion([FromBody] string text)
+        {
+            try
+            {
+                var question = new Question(text);
+                await db.Questions.Add(question);
+                return Ok(question);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
